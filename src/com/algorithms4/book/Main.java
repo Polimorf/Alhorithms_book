442dbcc5bef70ae6cd180d2d99cc9106ac93c3ce
polimorf <polimorf.dp@gmail.com>
2021-02-22 22:44:10 +0200

    new code

--- src/com/algorithms4/book/Main.java
+++ src/com/algorithms4/book/Main.java
@@ -7,9 +7,30 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] test = {11, 7, 28, 45, 63, 8, 5, 10, 110, 18, 23};
+        /*int[] test = {11, 7, 28, 45, 63, 8, 5, 10, 110, 18, 23};
         Arrays.sort(test);
         int result = BinarySearch.rank(8, test);
-        System.out.println("result = "+result);
+        System.out.println("result = "+result);*/
+
+        /*int N = Integer.parseInt(args[0]);
+        double lo = Double.parseDouble(args[1]);
+        double hi = Double.parseDouble(args[2]);
+        for(int i=0; i<N; i++)
+        {
+            double x = StdRandom.uniform(lo, hi);
+            StdOut.printf("%.2f\n", x);
+        }*/
+
+        double sum = 0.0;
+        int cnt = 0;
+        while(!StdIn.isEmpty())
+        {
+            sum += StdIn.readDouble();
+            cnt++;
+        }
+
+        double avg = sum / cnt;
+        StdOut.printf("Среднее - %.5f", avg);
+
     }
 }
