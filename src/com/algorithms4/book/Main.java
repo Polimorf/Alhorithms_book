25b25f14c5d9a793adf4f903ebde90c9bdfc5183
polimorf <polimorf.dp@gmail.com>
2021-02-22 19:38:27 +0200

    Bninary search

--- src/com/algorithms4/book/Main.java
+++ src/com/algorithms4/book/Main.java
@@ -1,11 +1,15 @@
 package com.algorithms4.book;
 
 import edu.princeton.cs.algs4.*;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World!");
+        int[] test = {11, 7, 28, 45, 63, 8, 5, 10, 110, 18, 23};
+        Arrays.sort(test);
+        int result = BinarySearch.rank(8, test);
+        System.out.println("result = "+result);
     }
 }
